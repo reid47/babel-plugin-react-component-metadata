7d8ecc48ff7009d9e6f67b638599eaaac6e149ee
Reid Mitchell <reidmitchell1@gmail.com>
2018-02-15 18:46:10 -0800

    Slight refactoring

--- src/index.js
+++ src/index.js
@@ -12,127 +12,126 @@ const init = (obj, ...fields) =>
   });
 
 const isTopLevel = path => path.parent.type === 'Program';
+
 const isExported = path =>
   path.parent.type === 'ExportNamedDeclaration' ||
   path.parent.type === 'ExportDefaultDeclaration';
 
-export default () => {
-  const collectPropTypes = (propMetadata, objExp) =>
-    objExp.properties.forEach(propNode => {
-      if (!t.isIdentifier(propNode.key)) return;
-      const propName = propNode.key.name;
+const collectPropTypes = (propMetadata, objExp) =>
+  objExp.properties.forEach(propNode => {
+    if (!t.isIdentifier(propNode.key)) return;
+    const propName = propNode.key.name;
 
-      init(propMetadata, propName);
-      propMetadata[propName] = propNode.value;
-    });
+    init(propMetadata, propName);
+    propMetadata[propName] = propNode.value;
+  });
 
-  return {
-    visitor: {
-      ImportDeclaration(path, state) {
-        if (path.node.source.value !== 'prop-types') return;
+export default () => ({
+  visitor: {
+    ImportDeclaration(path, state) {
+      if (path.node.source.value !== 'prop-types') return;
 
-        const defaultImport = path.node.specifiers.filter(s =>
-          t.isImportDefaultSpecifier(s)
-        )[0];
-        if (!defaultImport) return;
+      const defaultImport = path.node.specifiers.filter(s =>
+        t.isImportDefaultSpecifier(s)
+      )[0];
+      if (!defaultImport) return;
 
-        setOption('propTypesAlias', defaultImport.local.name);
+      setOption('propTypesAlias', defaultImport.local.name);
+    },
+
+    ClassDeclaration(path, state) {
+      if (!(isTopLevel(path) || isExported(path))) return;
+
+      init(state.knownComponents, path.node.id.name);
+      state.knownComponents[path.node.id.name].path = isExported(path)
+        ? path.parentPath
+        : path;
+    },
+
+    FunctionDeclaration(path, state) {
+      if (!(isTopLevel(path) || isExported(path))) return;
+
+      init(state.knownComponents, path.node.id.name);
+      state.knownComponents[path.node.id.name].path = isExported(path)
+        ? path.parentPath
+        : path;
+    },
+
+    VariableDeclaration(path, state) {
+      if (!(isTopLevel(path) || isExported(path))) return;
+
+      const declarator = path.node.declarations[0];
+      if (!t.isFunction(declarator.init)) return;
+
+      init(state.knownComponents, declarator.id.name);
+      state.knownComponents[declarator.id.name].path = isExported(path)
+        ? path.parentPath
+        : path;
+    },
+
+    ClassProperty(path, state) {
+      if (!path.node.static) return;
+
+      const staticPropName = path.node.key.name;
+      if (staticPropName !== 'propTypes') return;
+      const staticPropVal = path.node.value;
+      if (!t.isObjectExpression(staticPropVal)) return;
+
+      const parentClassDecl = path.findParent(p => t.isClassDeclaration(p));
+      const parentClassName = parentClassDecl.node.id.name;
+      if (!state.knownComponents[parentClassName]) return;
+
+      init(state.knownComponents[parentClassName], 'props');
+      collectPropTypes(
+        state.knownComponents[parentClassName].props,
+        staticPropVal
+      );
+    },
+
+    ExpressionStatement(path, state) {
+      if (!isTopLevel(path)) return;
+      if (!t.isAssignmentExpression(path.node.expression)) return;
+
+      const { left, right } = path.node.expression;
+      if (!t.isMemberExpression(left)) return;
+      if (!t.isObjectExpression(right)) return;
+      if (left.property.name !== 'propTypes') return;
+
+      const componentName = left.object.name;
+      init(state.knownComponents, componentName, 'props');
+      collectPropTypes(state.knownComponents[componentName].props, right);
+    },
+
+    Program: {
+      enter(path, state) {
+        state.knownComponents = {};
       },
 
-      ClassDeclaration(path, state) {
-        if (!(isTopLevel(path) || isExported(path))) return;
+      exit(path, state) {
+        let insertedHelpers = false;
 
-        init(state.knownComponents, path.node.id.name);
-        state.knownComponents[path.node.id.name].path = isExported(path)
-          ? path.parentPath
-          : path;
-      },
+        Object.keys(state.knownComponents).forEach(c => {
+          const componentPath = state.knownComponents[c].path;
+          const componentProps = state.knownComponents[c].props;
 
-      FunctionDeclaration(path, state) {
-        if (!(isTopLevel(path) || isExported(path))) return;
+          if (componentPath && componentProps) {
+            if (!insertedHelpers) {
+              const helpersName = path.scope.generateUidIdentifierBasedOnNode(
+                getOption(state, 'helpersName')
+              );
 
-        init(state.knownComponents, path.node.id.name);
-        state.knownComponents[path.node.id.name].path = isExported(path)
-          ? path.parentPath
-          : path;
-      },
-
-      VariableDeclaration(path, state) {
-        if (!(isTopLevel(path) || isExported(path))) return;
-
-        const declarator = path.node.declarations[0];
-        if (!t.isFunction(declarator.init)) return;
-
-        init(state.knownComponents, declarator.id.name);
-        state.knownComponents[declarator.id.name].path = isExported(path)
-          ? path.parentPath
-          : path;
-      },
-
-      ClassProperty(path, state) {
-        if (!path.node.static) return;
-
-        const staticPropName = path.node.key.name;
-        if (staticPropName !== 'propTypes') return;
-        const staticPropVal = path.node.value;
-        if (!t.isObjectExpression(staticPropVal)) return;
-
-        const parentClassDecl = path.findParent(p => t.isClassDeclaration(p));
-        const parentClassName = parentClassDecl.node.id.name;
-        if (!state.knownComponents[parentClassName]) return;
-
-        init(state.knownComponents[parentClassName], 'props');
-        collectPropTypes(
-          state.knownComponents[parentClassName].props,
-          staticPropVal
-        );
-      },
-
-      ExpressionStatement(path, state) {
-        if (!isTopLevel(path)) return;
-        if (!t.isAssignmentExpression(path.node.expression)) return;
-
-        const { left, right } = path.node.expression;
-        if (!t.isMemberExpression(left)) return;
-        if (!t.isObjectExpression(right)) return;
-        if (left.property.name !== 'propTypes') return;
-
-        const componentName = left.object.name;
-        init(state.knownComponents, componentName, 'props');
-        collectPropTypes(state.knownComponents[componentName].props, right);
-      },
-
-      Program: {
-        enter(path, state) {
-          state.knownComponents = {};
-        },
-
-        exit(path, state) {
-          let insertedHelpers = false;
-
-          Object.keys(state.knownComponents).forEach(c => {
-            const componentPath = state.knownComponents[c].path;
-            const componentProps = state.knownComponents[c].props;
-
-            if (componentPath && componentProps) {
-              if (!insertedHelpers) {
-                const helpersName = path.scope.generateUidIdentifierBasedOnNode(
-                  getOption(state, 'helpersName')
-                );
-
-                path.unshiftContainer('body', buildHelpers(helpersName));
-                insertedHelpers = true;
-              }
-
-              const newPath = componentPath.insertAfter(
-                createMetadataNode(c, componentProps, state)
-              )[0];
-
-              newPath.traverse(helpersVisitor, state);
+              path.unshiftContainer('body', buildHelpers(helpersName));
+              insertedHelpers = true;
             }
-          });
-        }
+
+            const newPath = componentPath.insertAfter(
+              createMetadataNode(c, componentProps, state)
+            )[0];
+
+            newPath.traverse(helpersVisitor, state);
+          }
+        });
       }
     }
-  };
-};
+  }
+});
